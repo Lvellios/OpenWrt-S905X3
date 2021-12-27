4f065a2021457fdac7e03482859dece809a48aad
Lvellios <35004794+Lvellios@users.noreply.github.com>
2021-12-27 10:10:13 -0400

    Update mk_s905x3_multi.sh

--- First/opt/openwrt_packit/mk_s905x3_multi.sh
+++ First/opt/openwrt_packit/mk_s905x3_multi.sh
@@ -30,7 +30,7 @@ check_file ${OPWRT_ROOTFS_GZ}
 echo "Use $OPWRT_ROOTFS_GZ as openwrt rootfs!"
 
 # 目标镜像文件
-TGT_IMG="${WORK_DIR}/OpenWrt_S905X3_5.10.88_68+.img"
+TGT_IMG="${WORK_DIR}/OpenWrt_S905X3_5.14.21_67+.img"
 
 # 补丁和脚本
 ###########################################################################
