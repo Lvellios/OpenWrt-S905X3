ac60946419839332f822da5e0623896fc128821b
Lvellios <35004794+Lvellios@users.noreply.github.com>
2022-02-06 04:25:16 -0400

    Upgrade

--- Second/opt/packit/mk_s905x3_multi.sh
+++ Second/opt/packit/mk_s905x3_multi.sh
@@ -30,7 +30,7 @@ check_file ${OPWRT_ROOTFS_GZ}
 echo "Use $OPWRT_ROOTFS_GZ as openwrt rootfs!"
 
 # 目标镜像文件
-TGT_IMG="${WORK_DIR}/OpenWrt_S905X3_5.10.92_69+.img"
+TGT_IMG="${WORK_DIR}/openwrt_${SOC}_${BOARD}_${OPENWRT_VER}_k${KERNEL_VERSION}${SUBVER}.img"
 
 # 补丁和脚本
 ###########################################################################
